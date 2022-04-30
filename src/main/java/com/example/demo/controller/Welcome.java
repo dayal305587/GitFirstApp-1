6f9bc89aece2c0fcae6174cf70ba7f8661d4ebec
dayal102687 <dayalsatguru@gmail.com>
2022-04-30 20:03:32 +0530

    added

---
+++ src/main/java/com/example/demo/controller/Welcome.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Simran Cafe");
+	}
+	
+	public void show() {
+		
+		System.out.println("Welcm to BikkadIt");
+	}
+
+}
